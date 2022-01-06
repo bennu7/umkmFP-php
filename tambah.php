52c4f300c96cb234e028a59a850581a40229168d
bennu7 <laluibnuhidayatullah@gmail.com>
2022-01-06 15:21:28 +0700

    lanjut2

--- tambah.php
+++ tambah.php
@@ -7,27 +7,25 @@
     <div class="card-body">
         <form action="" method="POST">
             <div class="form-group mb-3 ">
-                <label class="form-label">Nama Lengkap</label>
+                <label class="form-label">Nama Produk</label>
                 <div>
-                    <input type="text" class="form-control" aria-describedby="" placeholder="" readonly>
+                    <input type="text" class="form-control" aria-describedby="" placeholder="">
                 </div>
             </div>
             <div class="form-group mb-3 ">
-                <label class="form-label" for="email">Alamat Email</label>
+                <label class="form-label" for="email" placeholder="Masukkan Alamat Email" autocomplete="off">Deskripsi Produk</label>
                 <div>
-                    <input type="email" name="email" id="email" class="form-control" aria-describedby="emailHelp" placeholder="Masukkan Alamat Email" autocomplete="off">
+                    <input type="email" name="email" id="email" class="form-control" aria-describedby="emailHelp" placeholder="Masukkan Deskripsi Produk" autocomplete="off">
                     <div class="invalid-feedback"></div>
                 </div>
             </div>
             <div class="form-group mb-3 text-muted">
-                <label class="form-label" for="alamat">Alamat rumah</label>
+                <label class="form-label" for="alamat">Pilih Gambar</label>
                 <input type="file" name="alamat" id="alamat" class="form-control">
-                <!-- <textarea class="form-control" data-bs-toggle="autosize" placeholder="Type something…">jl. bersama dia tapi tidak pernah bersatu rt 01 rw 10 kec bersama kelurahan tidak pasti kabupaten mengsedih</textarea> -->
             </div>
             <div class="form-group mb-3 text-muted">
-                <label class="form-label" for="alamat">Alamat rumah</label>
+                <label class="form-label" for="alamat">Harga</label>
                 <input type="text" name="alamat" id="alamat" class="form-control">
-                <!-- <textarea class="form-control" data-bs-toggle="autosize" placeholder="Type something…">jl. bersama dia tapi tidak pernah bersatu rt 01 rw 10 kec bersama kelurahan tidak pasti kabupaten mengsedih</textarea> -->
             </div>
     </div>
     <div class="form-footer">
